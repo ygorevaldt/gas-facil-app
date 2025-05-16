34b2ca8bd3f71033af418c392ad70fb66351837e
ygorevaldt <evaldtygor@gmail.com>
2025-05-16 19:16:51 -0300

    add colors schema

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,7 +1,18 @@
 export const colors = {
-  blue: {},
-  gray: {},
-  green: {},
-  yellow: {},
-  red: {},
+  blue: {
+    200: "#dbe8f2",
+    500: "#4288b9",
+  },
+  gray: {
+    100: "#f3f3f3",
+  },
+  green: {
+    900: "#178b68",
+  },
+  yellow: {
+    500: "#e3bd00",
+  },
+  red: {
+    800: "#b94242",
+  },
 };
