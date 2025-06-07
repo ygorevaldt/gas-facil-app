462e997533a594dcf9c6e1a814826b637074a7c9
ygorevaldt <evaldtygor@gmail.com>
2025-06-07 11:24:03 -0300

    adiciona config para gerar apks

--- app.config.js
+++ app.config.js
@@ -3,8 +3,15 @@ export default {
     name: "gas-facil-app",
     slug: "gas-facil-app",
     scheme: "gas-facil-app",
+    version: "1.0.0",
+    android: {
+      package: "com.ygorevaldt.gasfacilapp",
+    },
     extra: {
       API_URL: process.env.API_URL,
+      eas: {
+        projectId: "eb5ac0cf-5ea6-4a68-83f8-d64f3b51dd80",
+      },
     },
   },
 };
